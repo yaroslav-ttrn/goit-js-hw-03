343f9e8e641505e18dfd019dd7f85fa674f51460
yaroslav_ttrn <yaroslavtataryn@gmail.com>
2025-07-09 16:49:42 +0300

    ain

---
+++ js/task-1.js
@@ -0,0 +1,10 @@
+function slugify(title) {
+  let slug = title.toLowerCase();
+  let slugLow = slug.split(' ');
+  let slugJo = slugLow.join('-');
+  return slugJo;
+}
+console.log(slugify('Arrays for beginners')); // "arrays-for-beginners"
+console.log(slugify('English for developer')); // "english-for-developer"
+console.log(slugify('Ten secrets of JavaScript')); // "ten-secrets-of-javascript"
+console.log(slugify('How to become a JUNIOR developer in TWO WEEKS')); // "how-to-become-a-junior-developer-in-two-weeks"
---
+++ js/task-2.js
@@ -0,0 +1,14 @@
+function makeArray(firsArray, secondArray, maxLength) {
+  let comb = firsArray.concat(secondArray);
+  if (comb.length > maxLength) {
+    return comb.slice(0, maxLength);
+  } else {
+    return comb;
+  }
+}
+console.log(makeArray(['Mango', 'Poly'], ['Ajax', 'Chelsea'], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(['Mango', 'Poly', 'Houston'], ['Ajax', 'Chelsea'], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(['Mango'], ['Ajax', 'Chelsea', 'Poly', 'Houston'], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus', 'Venus'], 0)); // []
